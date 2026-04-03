6be297b02d500ffe3c462f0dd906f7d5c6450cae
anacleto85 <umbrico.alessandro@gmail.com>
2026-04-03 12:30:33 +0200

    setting up context reasoner application

---
+++ ContextReasoner/mc/src/main/java/it/cnr/istc/pst/mc/ContextReasonerApplication.java
@@ -0,0 +1,16 @@
+package it.cnr.istc.pst.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Context reasoner of the MetaCognition 
+ */
+@SpringBootApplication
+public class ContextReasonerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextReasonerApplication.class, args);
+	}
+
+}
---
+++ ContextReasoner/mc/src/test/java/it/cnr/istc/pst/mc/McApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cnr.istc.pst.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
